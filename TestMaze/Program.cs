cd671381e677abf21b49750123550140892bfe7b
Vladimir Melee <falcon_v@mail.ru>
2020-10-13 10:40:07 +0300

    add TestProject

---
+++ TestMaze/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaze
+{
+    public struct genCell
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool Visited { get; set; }
+        public int Value { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
